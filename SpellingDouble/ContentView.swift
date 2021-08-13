7d7c007f97aa34004371fc5ad1b97783de4f3fc1
Harry Bryant <70177129+htbryantDev@users.noreply.github.com>
2021-08-13 15:36:29 +0100

    Initial Commit

---
+++ SpellingDouble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpellingDouble
+//
+//  Created by Harry Bryant on 13/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpellingDouble/SpellingDoubleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingDoubleApp.swift
+//  SpellingDouble
+//
+//  Created by Harry Bryant on 13/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpellingDoubleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
